48f99fb07b1e1a2cf87f599ae94f76ed7949e695
Abel Martín <abel121105@gmail.com>
2026-07-10 11:03:48 +0100

    Cambiada foto de fondo en login

--- frontend-banda/src/pages/Login.tsx
+++ frontend-banda/src/pages/Login.tsx
@@ -46,7 +46,7 @@ export default function Login() {
 
   return (
     // Contenedor principal con imagen de fondo y efecto de oscurecimiento
-    <div className="min-h-screen flex items-center justify-center relative bg-[url('https://images.unsplash.com/photo-1507838153414-b4b713384a76?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center">
+    <div className="min-h-screen flex items-center justify-center relative bg-[url('../../public/fondo-login.jpg')] bg-cover bg-center">
       
       {/* Capa negra semitransparente para que el texto resalte */}
       <div className="absolute inset-0 bg-slate-900/70 backdrop-blur-sm"></div>
